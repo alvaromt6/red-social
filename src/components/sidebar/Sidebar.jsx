92ab8d687399f06f8a8909037574b1f2f80ab74e
Alvaro Macias <alvaromacias614@gmail.com>
2025-08-12 13:40:12 +0200

    Diseño Sidebar
    Instalación de Iconify

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,70 @@
+import { NavLink } from "react-router-dom";
+import { Icon } from "@iconify/react";
+
 export const Sidebar = () => {
-  return (
-    <div className="h-screen bg-amber-300 text-black">
-      <span>Sidebar</span>
-    </div>
-  );
+    const links = [
+        {
+            label: "Inicio",
+            icon: "ic:baseline-home",
+            to: "/",
+        },
+        {
+            label: "Notificaciones",
+            icon: "ic:baseline-notifications",
+            to: "/notificaciones",
+        },
+        {
+            label: "Mensajes",
+            icon: "ic:baseline-message",
+            to: "/mensajes",
+        },
+        {
+            label: "Colecciones",
+            icon: "material-symbols:folder-rounded",
+            to: "/colecciones",
+        },
+        {
+            label: "Subscripciones",
+            icon: "ic:baseline-star",
+            to: "/subscripciones",
+        },
+        {
+            label: "Añadir tarjeta",
+            icon: "ic:baseline-credit-card",
+            to: "/tarjeta",
+        },
+        {
+            label: "Mi perfil",
+            icon: "ic:baseline-account-circle",
+            to: "/perfil",
+        },
+    ];
+
+    return (
+        <div className="h-screen p-2 bg-white dark:bg-bg-dark text-black dark:text-white transition-all duration-300 flex flex-col">
+            {/* logo */}
+            <div className="flex justify-center items-center h-8 w-8 rounded-full bg-blue-100 text-primary font-bold text-xs m-2">
+                OD
+            </div>
+            {/* NAV */}
+            <nav className="flex-1 flex flex-col gap-2 items-center">
+                {links.map((item, index) => (
+                    <NavLink
+                        key={index}
+                        to={item.to}
+                        className={({ isActive }) =>
+                            `flex items-center gap-3 p-2 rounded-lg font-semibold hover:bg-gray-100 dark:hover:bg-primary/10 dark:hover:text-primary transition-all w-full justify-center sm:justify-start ${
+                                isActive
+                                    ? "bg-gray-200 dark:bg-primary/20 text-primary"
+                                    : "text-gray-600 dark:text-gray-400"
+                            }`
+                        }
+                    >
+                        <Icon icon={item.icon} width={24} height={24} />
+                        <span className="hidden sm:block">{item.label}</span>
+                    </NavLink>
+                ))}
+            </nav>
+        </div>
+    );
 };
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,9 +3,13 @@ import { Sidebar } from "../components/sidebar/Sidebar";
 
 export const MainLayout = () => {
   return (
-    <div className="h-screen bg-black text-white">
-      <Sidebar/>
-      <Outlet/>
-    </div>
+    <main className="flex justify-center h-screen overflow-hidden bg-white dark:bg-bg-dark text-black dark:text-white transition-colors duration-300">
+      <section className="flex w-full max-w-[1300px]">
+        <Sidebar />
+        <section className="flex-1 px-4 overflow-y-auto h-full">
+          <Outlet />
+        </section>
+      </section>
+    </main>
   );
 };
