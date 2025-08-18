fc2235188bed553671458b7c81842cba7bae3a48
Alvaro Macias <alvaromacias614@gmail.com>
2025-08-18 21:09:26 +0200

    Cambio la query al Main Layout que envuelve a todo, y en miperfil hacemos referencia a la data del usuario

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components/sidebar/Sidebar";
+import { useMostrarUsuariosQuery } from "../stack/UsuariosStack";
 
 export const MainLayout = () => {
+  const { data, isLoading, error } = useMostrarUsuariosQuery();
+    if (isLoading) return <div>Cargando...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+    if (!data) return <div>No hay datos disponibles</div>;
+
   return (
     <main className="flex justify-center h-screen overflow-hidden bg-white dark:bg-bg-dark text-black dark:text-white transition-colors duration-300">
       <section className="flex w-full max-w-[1300px]">
--- src/pages/MiPerfilPage.jsx
+++ src/pages/MiPerfilPage.jsx
@@ -1,15 +1,11 @@
-import { useMostrarUsuariosQuery } from "../stack/UsuariosStack";
+import { useUsuariosStore } from "../store/UsuarioStore";
 
 export const MiPerfilPage = () => {
-    const { data, isLoading, error } = useMostrarUsuariosQuery();
-    if (isLoading) return <div>Cargando...</div>;
-    if (error) return <div>Error: {error.message}</div>;
-    if (!data) return <div>No hay datos disponibles</div>;
-
+  const { dataUsuario } = useUsuariosStore();
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-bg-dark max-w-[1200px] mx-auto p-4 text-black dark:text-white">
       <span>MiPerfilPage</span>
-      <span>{data?.nombre}</span>
+      <span>{dataUsuario?.nombre}</span>
     </div>
   );
 };
