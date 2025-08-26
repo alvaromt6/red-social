1f6293922fedbf93a74c3f7718d088ca6460a54f
Alvaro Macias <alvaromacias614@gmail.com>
2025-08-26 21:54:27 +0200

    feat(BtnClose): añadir prop función para manejar evento de cierre

--- src/components/sidebar/ui/BtnClose.jsx
+++ src/components/sidebar/ui/BtnClose.jsx
@@ -1,9 +1,11 @@
 import { Icon } from "@iconify/react";
 
-export const BtnClose = () => {
+export const BtnClose = ({funcion}) => {
   return (
-    <div className="absolute top-3 right-3 cursor-pointer p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors">
-      <Icon icon="material-symbols:close" width={20} height={20} />
+    <div 
+      onClick={funcion}
+      className="absolute top-3 right-3 cursor-pointer p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors">
+        <Icon icon="material-symbols:close" width={20} height={20} />
     </div>
   );
 }; 
