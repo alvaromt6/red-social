ba6541ddf7d4b786b64b2b7ce44936d214706b2a
Alvaro Macias <alvaromacias614@gmail.com>
2025-09-05 12:07:49 +0200

    Instalacion de Spinners de carga
    npm install --save react-spinners

---
+++ src/components/sidebar/ui/spinners/SpinnerMoonloader.jsx
@@ -0,0 +1,9 @@
+import { MoonLoader } from "react-spinners";
+
+export const SpinnerMoonloader = () => {
+  return (
+    <div className="w-full flex justify-center items-center p-6">
+      <MoonLoader color="#c8c8c8" size={25} />
+    </div>
+  );
+};
