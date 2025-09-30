bf3c3ebde5136d3ae910eba701721d422aaeec3f
Alvaro Macias <alvaromacias614@gmail.com>
2025-09-30 17:47:38 +0200

    feat: añadir hooks de React Query para respuestas a comentarios
    - Crear mutation para insertar respuestas
    - Implementar query para mostrar respuestas de comentario
    - Integrar notificaciones toast y limpieza de estado

---
+++ src/stack/RespuestasComentariosStack.jsx
@@ -0,0 +1,48 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRespuestasComentariosStore } from "../store/RespuestasComentariosStore";
+import { useFormattedDate } from "../hooks/useFormattedDate";
+import { useUsuariosStore } from "../store/UsuarioStore";
+import { toast } from "sonner";
+import { useComentarioStore } from "../store/ComentarioStore";
+
+export const useInsertarRespuestaComentarioMutate = () => {
+  const {
+    insertarRespuestaComentarios,
+    respuestaActivaParaComentarioId,
+    respuesta,
+    setRespuesta,limpiarRespuestaActiva
+  } = useRespuestasComentariosStore();
+  const { dataUsuarioAuth } = useUsuariosStore();
+  const fechaAtual = useFormattedDate();
+  return useMutation({
+    mutationKey: ["insertar respuesta a comentario"],
+    mutationFn: () =>
+      insertarRespuestaComentarios({
+        id_comentario: respuestaActivaParaComentarioId,
+        comentario: respuesta,
+        fecha: fechaAtual,
+        id_usuario: dataUsuarioAuth?.id,
+      }),
+    onError: (error) => {
+      toast.error("Error al insertar respuesta: " + error.message);
+    },
+    onSuccess: () => {
+      toast.success("Respuesta enviada");
+      setRespuesta("");
+      limpiarRespuestaActiva()
+    },
+  });
+};
+export const useMostrarRespuestaComentariosQuery = () => {
+  const { mostrarRespuestaAComentario } = useRespuestasComentariosStore();
+  const { itemSelect } = useComentarioStore();
+  return useQuery({
+    queryKey: [
+      "mostrar respuesta comentario",
+      { id_comentario: itemSelect?.id },
+    ],
+    queryFn: () =>
+      mostrarRespuestaAComentario({ id_comentario: itemSelect?.id }),
+    enabled: !!itemSelect,
+  });
+};
